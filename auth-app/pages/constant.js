ad45018bf60537c2649af1f72ebd8190bdb33dea
Ayesha Shahzad <143947577+twiddling-thumbs@users.noreply.github.com>
2024-08-16 11:21:02 +0500

    Update constant.js

--- auth-app/pages/constant.js
+++ auth-app/pages/constant.js
@@ -1,6 +1,6 @@
 export const API_URL = "https://volta-rpc.energyweb.org/";
-export const PRIVATE_KEY ="0d41a7bc9850c50a0a597ebfda0afc4c20d61a91c79b7a6e48107892d0f5fad9";
-export const contractAddress = "0x7222Fa19668998941Ac4C82033AA5c517B95beA9";
+export const PRIVATE_KEY ="";
+export const contractAddress = "";
 export const contractAbi = 
 [
 	{
